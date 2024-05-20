c7060c8184f43bc18eea4b56b53ce46f15acb068
Camila <meusestudoson@gmail.com>
2024-05-19 22:39:51 -0300

    fix: adicionar menu de navegacao na tela de registro e configuracoes de perfil

--- pequenos-passos-serverside/resources/views/professor/registro2.blade.php
+++ pequenos-passos-serverside/resources/views/professor/registro2.blade.php
@@ -5,8 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('lib/css/materialize.min.css') }}">
-    <link rel="stylesheet" href="{{asset('css/global.css')}}">
-    <link rel="stylesheet" href="{{asset('css/main.css')}}">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>Novo Registro</title>
 </head>
@@ -34,72 +32,60 @@
   <ul class="sidenav" id="mobile-demo">
     <li><a href="">Sair <i class="material-icons">logout</i></a></li>
   </ul> -->
-
-  <header>
-    <div class="container">
-      <Button>
-        <a href="{{route('home.index')}}"><img src="{{asset('img/back.png')}}"></a>
-      </Button>
+<div class="container">
+            @php
+            $data_nascimento=\Carbon\Carbon::parse($aluno->data_nascimento);
+            $idade=floor($data_nascimento->diffInYears(\Carbon\Carbon::now()));
+            @endphp
+    <div class="row center">
+        <h6>MINI MATERNAL</h6>
     </div>
-  </header>
-  
-    <section>
-        <strong>MINI MATERNAL</strong>
-        <div class="img-profile">
-            @if($aluno->foto_aluno != '')
-            <img src="{{ url('storage/'.$aluno->foto_aluno) }}">
-             @else
-            <img src="{{asset('img/default/avatar.jpg')}}">
-            @endif
-        </div>
+    @if($aluno->foto_aluno != '')
+    <div class="row center" style="display:flex; justify-content:center;">
+        <img src="{{ url('storage/'.$aluno->foto_aluno) }}" width="200px" height="200px" class="circle" alt="imagem do aluno">
+    </div>
+    @else
+    <div class="row center" style="display:flex; justify-content:center;">
+        <img src="{{asset('img/default/avatar.jpg')}}" width="200px" height="200px" class="circle" alt="imagem do aluno">
+    </div>
+    @endif
 
-        <div class="profile-info">
-            @php
-            $data_nascimento=\Carbon\Carbon::parse($aluno->data_nascimento);
-            $idade=floor($data_nascimento->diffInYears(\Carbon\Carbon::now()));
-            @endphp
-            <strong>{{ $aluno->nome .' '.$aluno->sobrenome }}</strong>
-             <span>{{ $idade }} anos</span>
-         </div>
-    </section>
-
-  <div class="container">
-            @php
-            $data_nascimento=\Carbon\Carbon::parse($aluno->data_nascimento);
-            $idade=floor($data_nascimento->diffInYears(\Carbon\Carbon::now()));
-            @endphp
-
-     <form action="{{ route('registro.store',['ano'=>$bimestre->ano_letivo,'id'=>$aluno_id]) }}" method="POST">
-        @csrf
-        <div class="row">
+    <div class="row center">
+            <h6>{{ $aluno->nome .' '.$aluno->sobrenome }}</h6>
+            <h6>@php echo $idade; @endphp anos</h6>
+    </div>
+    <form action="{{ route('registro.store',['ano'=>$bimestre->ano_letivo,'id'=>$aluno_id]) }}" method="POST">
+    @csrf
+    <div class="row">
             <div class="input-field col s12">
                 <input type="date" name="data_registro" id="data_registro" style="color:gray;">
                 <label for="data_registro">Dia do registro</label>
             </div>
         </div>
-        <div class="row">
-            <div class="input-field s12">
-                <select name="eixo" id="">
-                    @foreach($criterios as $criterio)
-                    <option value="{{ $criterio->nome }}">{{ $criterio->nome }}</option>
-                    @endforeach
-                </select>
-            </div>
-        </div>    
+    <div class="row">
+        <div class="input-field s12">
+            <select name="eixo" id="">
+        @foreach($criterios as $criterio)
+                <option value="{{ $criterio->nome }}">{{ $criterio->nome }}</option>
+        @endforeach
+            </select>
+        </div>
+    </div>
+    
     <div class="row">
         <div class="input-field s12">
             <input type="text" name="texto" id="registro_texto">
             <label for="registro_texto">Observação</label>
         </div>
     </div>
-        <div class="row center" style="width:100%; display:flex; flex-wrap:nowrap; justify-content:space-around;">
+    <div class="row center" style="width:100%; display:flex; flex-wrap:nowrap; justify-content:space-around;">
             <a href="" class="btn" style="width:45%;">Cancelar</a>
             <button type="submit" class="btn orange" style="width:45%;">Salvar</button>
-        </div>
+    </div>
     </form>
 </div>
-</div>
   
+    
     <script src="{{ asset('lib/js/materialize.min.js') }}"></script>
     <script src="{{ asset('js/script.js') }}"></script>
 </body>
